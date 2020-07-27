9b40e0bcd1cc8d9ff811d806f4023a3342e5125d
Colin White <colin@colinwhite.me>
2020-07-27 15:28:36 -0400

    Work around ktlint issue.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ buildscript {
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.11.1")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
         classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.2.3")
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.2.1")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.3.0")
         classpath(kotlin("gradle-plugin", version = "1.4.0-rc"))
     }
 }
@@ -50,6 +50,11 @@ allprojects {
     extensions.configure<KtlintExtension>("ktlint") {
         version.set("0.37.2")
         enableExperimentalRules.set(true)
+        disabledRules.set(setOf("experimental:annotation", "import-ordering", "indent", "max-line-length"))
+        filter {
+            // https://github.com/pinterest/ktlint/issues/726
+            exclude("**/ImageLoaderFactory.kt")
+        }
     }
 
     tasks.withType<KotlinCompile> {
